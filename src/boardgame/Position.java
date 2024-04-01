ce627e38c0cd9923b7af8bb51301fac19c8f68e3
lacerdajcarlos <lacerda.jcarlos@gmail.com>
2024-04-01 07:45:46 +0100

    Chess logics

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,12 +1,12 @@
 package boardgame;
 
 public class Position {
-	
+
 	private int row;
 	private int column;
-	
+
 	public Position(int row, int column) {
-		
+
 		this.row = row;
 		this.column = column;
 	}
@@ -26,6 +26,7 @@ public class Position {
 	public void setColumn(int column) {
 		this.column = column;
 	}
+
 	public void setValues(int row, int column) {
 		this.row = row;
 		this.column = column;
@@ -33,13 +34,7 @@ public class Position {
 
 	@Override
 	public String toString() {
-		return  row + ", " + column;
+		return row + ", " + column;
 	}
-	
-	
-	
-	
-	
-	
 
 }
